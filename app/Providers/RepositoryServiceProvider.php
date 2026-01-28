f119643bcd2dc4bc0d028236b85db9f4ce0823ed
Alfredo Patricius Tarigan <alfredoptarigan@gmail.com>
2026-01-28 08:36:51 +0700

    initialize project

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        // $this->app->bind(
+        //     \App\Interfaces\User\UserRepositoryInterface::class,
+        //     \App\Repositories\User\UserRepository::class
+        // );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
